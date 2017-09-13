05be60b328b06370edc9bb7c84739dbb250296b3
Brian Nguyen <bhn2600@gmail.com>
2017-09-12 18:28:47 -0700

    Prelim submission and linked challenge

--- Challenges/Brian-09122017.py
+++ Challenges/Brian-09122017.py
@@ -1,59 +1,12 @@
-def circularArrayLoop(self, nums):
-    
-    # N time to loop through "starting indexes"
-    for x in range(len(nums)):
+def reverse(btree, p, acc):
+    for child,parent in btree:
+        if parent == p and p not in acc:
+            acc += child
+            return reverse(btree, child, acc)
 
-        """
-        start: Determines the start of the potential loop
-        direction: Flag to decide forward/backward direction
-        index: Current index to compart to start
-        counter: Checks if you've iterated at least once
-        checked: Amount of indexes checked. If greater than len(nums), break
-        """
-        start = x
-        direction = 0
-        index = x
-        counter = 0
-        checked = 0
+    if p not in acc:
+        return acc.append(p)
+    else:
+        return acc
 
-        # Checks the direction at the start index
-        if nums[x] > 0:
-            direction = 1
-        else:
-            direction = -1
-        
-        # Checked every index again (Another N so a total of O(N^2))
-        while True:
-            if checked > len(nums):
-                break
-
-            # Increment the counters/index
-            index = (index+nums[index])
-            counter += 1
-            checked += 1
-                
-            # Checks if you go past the border of the list to wrap around
-            if index > len(nums)-1:
-                index = index-len(nums)
-            elif index < 0:
-                index = index+len(nums)
-
-            # Checks to see if you change direction thus automatically failing
-            if direction == 1 and nums[index] < 0:
-                break
-            elif direction == -1 and nums[index] > 0:
-                break  
-
-            # Checks to see if you fully connected the loop and is greater than 1 otherwise break
-            if index == start and counter <= 1:
-                break
-            elif index == start and counter > 1:
-                return True
-        
-    return False
-
-if __name__ == "__main__":
-    n = int(raw_input().strip())
-    nums = map(int, raw_input().strip().split(' '))
-    results = circularArrayLoop(nums)
-    print results
+print reverse(tree, 'B', [])
--- Challenges/README.md
+++ Challenges/README.md
@@ -6,3 +6,4 @@ Date Submitted | Due Date     | Submitter | Platform    | Link
 08/31/2017     | 09/06/2017   | Kevin     | HackerRank  | [Link](https://www.hackerrank.com/challenges/between-two-sets)
 09/06/2017     | 09/08/2017   | Brian     | HackerRank  | [Link](https://www.hackerrank.com/challenges/migratory-birds/problem)
 09/08/2017     | 09/12/2017   | Kevin     | LeetCode    | [Link](https://leetcode.com/problems/circular-array-loop/description/)
+09/12/2017     | 09/15/2017   | Brian     | Personal    | [Link](https://github.com/Fabulinux/Project-Cognizant/blob/master/Challenges/PersonalSubmissions/Challenge-09122017.txt)
