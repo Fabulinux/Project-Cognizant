d7c66110a5e8cf182d8b431b40eaec8a11df70b5
Brian Nguyen <bhn2600@gmail.com>
2017-09-14 20:00:49 -0700

    Brians submission for reverse parent tree

--- Challenges/Brian-09122017.py
+++ Challenges/Brian-09122017.py
@@ -1,17 +1,28 @@
-def reverse(btree, p, acc):
+def reverse(tree, node, acc):
+    # Store the tree to remove checked nodes
     tmp = btree
-    for child,parent in btree:
+
+    # Extract the values from all the tree
+    for child,parent in tree:
+        # If root, skip
         if parent == None:
             continue
+
+        # Remove the head and only check what's left
         tmp = tmp[1:]
-        if parent == p and child not in acc:
+
+        # If parent value is deired root node, recurse and check it's children
+        if parent == node and child not in acc:
             acc.add(child)
             acc.union(reverse(tmp, child, acc))
 
+    # Add the node if it's not already
     acc.add(p)
     return acc
 
 tree = [('A', None), ('B', 'A'), ('C', 'A'), ('D', 'B'), ('E', 'B'), ('F', 'C'), ('G', 'E')]
-
-acc = set([])
-print list(reverse(tree, 'A', acc))
+if __name__ == "__main__":
+    node = str(raw_input().strip())
+    tree = raw_input()
+    acc = set([])
+    print list(reverse(tree, node, acc))
